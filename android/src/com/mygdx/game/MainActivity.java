158cda64b5de0a3bf9eccf00c9932953c1e33aac
zaclimon <isaacpateau05@gmail.com>
2018-02-25 06:14:08 -0500

    Merge with MuzeBoxes part two
    - Adapt MainActivity
    - Import ConnectActivity
    - AndroidManifest.xml: Add Bluetooth permissions

--- android/src/com/mygdx/game/MainActivity.java
+++ android/src/com/mygdx/game/MainActivity.java
@@ -1,8 +1,42 @@
 package com.mygdx.game;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
 /**
  * Created by isaac on 2018-02-25.
  */
 
-public class MainActivity extends  {
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button connectButton = findViewById(R.id.connect_button);
+
+        connectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ConnectActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button playButton = findViewById(R.id.play_button);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,AndroidLauncher.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
 }
